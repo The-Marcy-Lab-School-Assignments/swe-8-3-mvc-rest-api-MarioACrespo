96bc72961d38da51ebb7e254ef7854599189c1c9
Mario <airbomberofaqua@gmail.com>
2025-04-13 23:46:16 -0400

    public done

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const fellowController = require("./controllers/fellowControllers");
 
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "public")));
+
 app.use("/api/fellows", fellowController);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
